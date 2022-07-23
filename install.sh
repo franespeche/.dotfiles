fdf4fd1628653327d9f79d44d96a1a66d9aae473
frean <fespeche@gmail.com>
2022-07-23 19:14:17 -0300

    install comments

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 # install exa
 # install neovim
 # install lazygit
+# install tldr
 # make symlink (or copy) .zshenv to ~
 # sudo pacman -S cargo
 # ln -s ~/.dotfiles/zsh/.zshenv ~
+# change default shell: chsh -s $(which zsh)
+# may also need to comment out ~/.bashrc and exec zsh from there: $ exec zsh
 # creates symlinks
 # TODO: make these dynamic
 
@@ -25,3 +28,9 @@ echo "created symlink: nvim/"
 # git config --global pull.rebase true
 # git config --global fetch.prune true
 # git config --global diff.colorMoved zebra
+# vim
+# install vim-plug:
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
