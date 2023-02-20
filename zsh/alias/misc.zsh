3cf1380078cec5543c227b879737472a11c6ff44
frean <fespeche@gmail.com>
2023-02-20 02:05:09 -0300

    add aliases

--- zsh/alias/misc.zsh
+++ zsh/alias/misc.zsh
@@ -4,6 +4,7 @@
 
 alias rmrf='rm -rf $1'
 alias cdz="changeDirectory $DOTFILES/zsh"
+alias cdn="changeDirectory $DOTFILES/nvim"
 alias cdd="changeDirectory $DOTFILES"
 alias realvim="vim"
 alias vim="nvim"
@@ -19,6 +20,7 @@ alias va='nvim $ZDOTDIR/alias/misc.zsh'
 alias vz='nvim $ZDOTDIR/.zshrc'
 alias kan='killall -9 node'
 alias ac="arduino-cli"
+alias meteo="curl wttr.in/Buenos%20Aires"
 
 # vi git
 function vg() {
