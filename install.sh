5239537a2a6672226f96ff31f5b52918a9f7b3be
frean <fespeche@gmail.com>
2023-08-07 09:30:56 -0300

    updates

--- install.sh
+++ install.sh
@@ -37,3 +37,6 @@ echo "created symlink: nvim/"
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' 
 # install prettier globally to avoid bugs in neovim 
 # npm install -g prettier
+
+# Rust
+# CocInstall coc-rust-analyzer
