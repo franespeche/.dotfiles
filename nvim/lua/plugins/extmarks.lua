55d6019bbb8f16c760d3876496809418a32248ea
Fran Espeche <frean@Frans-MacBook-Pro.local>
2024-01-14 16:17:18 -0300

    base working

--- nvim/lua/plugins/extmarks.lua
+++ nvim/lua/plugins/extmarks.lua
@@ -20,15 +20,21 @@ local function wipe_extmarks(ns)
     vim.api.nvim_buf_del_extmark(buf, ns, mark[1])
   end
 end
--- Keymaps
--- toggle extmark
-vim.keymap.set({ "n", "v" }, "ms", function()
+
+-- Toggles selected range in Visual mode or single line in Normal mode
+local toggle_highlights = function()
   local mode = vim.api.nvim_get_mode().mode
   if (mode:lower() == "v" or mode == "\22") then
-    -- local start_line = vim.fn.line("'<")
-    local end_line = vim.fn.line("'>")
-    -- local opts = vim.tbl_extend('force', DEFAULT_OPTS, { end_row = end_line + 1 })
-    P(end_line)
+    local edge1 = vim.fn.getpos("v")[2]
+    local edge2 = vim.fn.getcurpos(0)[2]
+    local start_line = math.min(edge1, edge2)
+    local end_line = math.max(edge1, edge2)
+    local lines_range = end_line - start_line
+    for i = start_line, start_line + lines_range, 1 do
+      vim.api.nvim_buf_set_extmark(0, namespace, i - 1, 0, DEFAULT_OPTS)
+    end
+    -- exit Visual mode
+    vim.api.nvim_input("<esc>")
   else
     local pos = vim.api.nvim_win_get_cursor(0)
     local extmarks = vim.api.nvim_buf_get_extmarks(buf, namespace, { pos[1] - 1, 0 }, { pos[1] - 1, -1 }, {})
@@ -38,7 +44,11 @@ vim.keymap.set({ "n", "v" }, "ms", function()
       vim.api.nvim_buf_set_extmark(0, namespace, pos[1] - 1, 0, DEFAULT_OPTS)
     end
   end
-end, { silent = true })
+end
 
--- wipe extmarks
-vim.keymap.set({ "n", "v" }, "md", function() wipe_extmarks(namespace) end, { silent = true })
+-- Create autocommands
+vim.api.nvim_create_user_command("HighlightToggle", function() return toggle_highlights() end, { nargs = "?" })
+
+-- Keymaps
+vim.keymap.set({ "n", "v" }, "<leader>hs", function() vim.cmd(":HighlightToggle") end, { silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>hd", function() wipe_extmarks(namespace) end, { silent = true })
