321206efa38bf43b958cc7dc9700fe4f389c7328
Fran Espeche <frean@Frans-MBP.fibertel.com.ar>
2025-02-24 11:40:14 -0300

    test

--- nvim/lua/plugins/blame.lua
+++ nvim/lua/plugins/blame.lua
@@ -7,26 +7,35 @@ local api = vim.api
 local blame_ns = api.nvim_create_namespace("minimal_line_blame")
 local blame_cache = {}
 
-local function get_blame_info(bufnr, lnum)
+local function compute_blame_for_buffer(bufnr)
   local filepath = api.nvim_buf_get_name(bufnr)
-  local cmd = string.format("git -C %s blame -L%d,%d --porcelain -- %s",
-    vim.fn.fnamemodify(filepath, ":h"), lnum, lnum,
-    filepath)
+  local cmd = string.format("git -C %s blame --porcelain -- %s",
+    vim.fn.fnamemodify(filepath, ":h"), filepath)
   local result = vim.fn.systemlist(cmd)
 
-  if vim.v.shell_error ~= 0 or vim.tbl_isempty(result) then return nil end
+  if vim.v.shell_error ~= 0 or vim.tbl_isempty(result) then return end
 
-  local author = result[2]:match("^author (.+)$") or "Unknown"
-  local date = result[3]:match("^author%-time (.+)$")
-  local summary
+  blame_cache[bufnr] = {}
+  local line_number = 0
+
   for _, line in ipairs(result) do
-    summary = line:match("^summary (.+)$")
-    if summary then break end
+    if line:match("^author ") then
+      line_number = line_number + 1
+      local author = line:match("^author (.+)$") or "Unknown"
+      local date = result[_ + 1]:match("^author%-time (.+)$")
+      local summary
+      for i = _ + 2, #result do
+        summary = result[i]:match("^summary (.+)$")
+        if summary then break end
+      end
+      summary = summary or ""
+      blame_cache[bufnr][line_number] = string.format("%s • %s • %s",
+        author, os.date(
+          "%Y-%m-%d",
+          tonumber(date)),
+        summary)
+    end
   end
-  summary = summary or ""
-
-  return string.format("%s • %s • %s", author,
-    os.date("%Y-%m-%d", tonumber(date)), summary)
 end
 
 function M.show_blame()
@@ -37,14 +46,8 @@ function M.show_blame()
 
   api.nvim_buf_clear_namespace(bufnr, blame_ns, 0, -1)
 
-  -- Check cache first
-  if not blame_cache[bufnr] then blame_cache[bufnr] = {} end
-
-  local blame = blame_cache[bufnr][line_nr]
-  if not blame then
-    blame = get_blame_info(bufnr, line_nr)
-    blame_cache[bufnr][line_nr] = blame
-  end
+  -- Use cached blame information
+  local blame = blame_cache[bufnr] and blame_cache[bufnr][line_nr]
 
   if blame then
     api.nvim_buf_set_extmark(bufnr, blame_ns, line_nr - 1, -1, {
@@ -77,6 +80,13 @@ end
 
 function M.setup()
   vim.g.line_blame = config.get("line_blame")
+  api.nvim_create_autocmd("BufReadPost", {
+    callback = function ()
+      local bufnr = api.nvim_get_current_buf()
+      compute_blame_for_buffer(bufnr)
+    end,
+  })
+
   api.nvim_create_autocmd({
     "CursorHold",
     "CursorMoved",
@@ -87,7 +97,7 @@ function M.setup()
   api.nvim_create_autocmd("BufWritePost", {
     callback = function ()
       local bufnr = api.nvim_get_current_buf()
-      blame_cache[bufnr] = nil -- Invalidate cache on buffer write
+      compute_blame_for_buffer(bufnr) -- Recompute blame on buffer write
     end,
   })
 
