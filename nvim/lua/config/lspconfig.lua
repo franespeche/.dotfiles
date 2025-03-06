a909172e4e9737729ce83b34759d139571871f15
Fran Espeche <fran.espeche@scale.com>
2025-03-06 18:05:52 -0300

    add poetry venv path + lua things lsp configs

--- nvim/lua/config/lspconfig.lua
+++ nvim/lua/config/lspconfig.lua
@@ -8,6 +8,8 @@ local on_attach = function (client, bufnr)
   end
 end
 
+local POETRY_VENV_PATH = os.getenv("POETRY_VENV_PATH")
+
 -- lua
 lspconfig.lua_ls.setup {
   on_attach = on_attach,
@@ -23,6 +25,36 @@ lspconfig.lua_ls.setup {
       completion = {
         callSnippet = "Replace",
       },
+      workspace = {
+        library = {
+          vim.fn.expand("$VIMRUNTIME/lua"),
+          vim.fn.stdpath("config") .. "/lua",
+        },
+        maxPreload = 2000,
+        preloadFileSize = 50000,
+      },
+      -- Optionally configure module path mapping
+      runtime = {
+        -- LuaJIT in the case of Neovim
+        version = "LuaJIT",
+        -- Setup your lua path
+        path = vim.split(package.path, ";"),
+      },
+      -- Telemetry settings (optional)
+      telemetry = {
+        enable = false,
+      },
     },
+  },
+}
+
+lspconfig.pyright.setup {
+  root_dir = POETRY_VENV_PATH or lspconfig.pyright.root_dir,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+      }
+    }
   },
 }
