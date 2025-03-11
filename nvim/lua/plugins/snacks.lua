517770a19ba0fdf941a68e1995fda5246a20116c
Fran Espeche <frean@Frans-MacBook-Pro.local>
2025-03-11 09:04:13 -0300

    lazygit theme fix

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -91,9 +91,44 @@ return {
     layout = {
       enabled = false,
      },
-    -- Open LazyGit in a float, auto-configure colorscheme and integration with Neovim	
+    -- Lazygit
     lazygit = {
       enabled = true,
+      theme = {
+        [241] = {
+          fg = "Special",
+         },
+        activeBorderColor = {
+          fg = "MatchParen",
+          bold = true,
+         },
+        cherryPickedCommitBgColor = {
+          fg = "Identifier",
+         },
+        cherryPickedCommitFgColor = {
+          fg = "Function",
+         },
+        defaultFgColor = {
+          fg = "Normal",
+         },
+        inactiveBorderColor = {
+          fg = "FloatBorder",
+         },
+        optionsTextColor = {
+          fg = "Function",
+         },
+        searchingActiveBorderColor = {
+          fg = "MatchParen",
+          bold = true,
+         },
+        selectedLineBgColor = {
+          fg = "Search",
+          bg = "Visual",
+         }, -- set to `default` to have no background colour
+        unstagedChangesColor = {
+          fg = "DiagnosticError",
+         },
+       },
      },
     -- Pretty vim.notify	
     notifier = {
