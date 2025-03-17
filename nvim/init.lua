641ef1c60adf64608a93b44178b0451ac72209a4
Fran Espeche <frean@Frans-MacBook-Pro.local>
2025-03-17 00:27:44 -0300

    remove comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,6 @@ vim.g.mapleader = ","
 vim.g.debug_mode = false
 vim.g.config_path = "/config.yaml"
 
--- TODO: move this somewhere else (is this needed btw?)
 vim.g["prettier#autoformat"] = 1 -- enable auto formatting of files that have "@format" or "@prettier" tag
 vim.g["prettier#autoformat_require_pragma"] = 1 -- allow auto formatting for files without "@format" or "@prettier" tag
 vim.g["prettier#autoformat_config_present"] = 1 -- toggle the g:prettier#autoformat setting based on whether a config file can be found in the current directory or any parent directory. Note that this will override the g:prettier#autoformat setting!
