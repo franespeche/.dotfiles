5253d257a58284d7ae1c60ff5caf06c310411219
Fran Espeche <fran.espeche@scale.com>
2025-05-12 10:49:50 -0300

    add dev

--- nvim/lua/plugins/dev.lua
+++ nvim/lua/plugins/dev.lua
@@ -1,18 +1,23 @@
 return {
+  {
+    "franespeche/quickfix-lint"
+  },
+
   -- lua
   {
     "folke/lua-dev.nvim",
-   },
+  },
   {
     "rafcamlet/nvim-luapad",
-   },
+  },
   {
     "milisims/nvim-luaref",
-   },
+  },
 
   -- vim
   {
     -- debugging tools
     "tpope/vim-scriptease",
-   },
- }
+  },
+
+}
