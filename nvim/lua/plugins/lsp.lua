a2335e8047040facf27b09e0811abcb0a7f73d35
frean <fespeche@gmail.com>
2025-08-24 16:03:27 -0300

    slight ref

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,42 +4,19 @@ local function show_documentation()
   local cword = vim.fn.expand("<cword>")
 
   if vim.fn.index({
-      "help",
-    }, filetype) >= 0 then
+    "help",
+   }, filetype) >= 0 then
     vim.cmd("h " .. cword)
   else
     if vim.fn.index({
-        "lua",
-      }, filetype) >= 0 then
+      "lua",
+     }, filetype) >= 0 then
       vim.lsp.buf.hover()
     else
       vim.fn.CocAction("doHover")
     end
   end
 end
-
--- restart
-vim.keymap.set("n", "<leader>cr", ":CocRestart<cr> ", Opts)
-vim.keymap.set("n", "<leader>tr", ":CocCommand tsserver.restart<cr>", Opts)
-
--- either doHover or show documentation in preview window
-vim.keymap.set("n", "<leader>ck", function () show_documentation() end, Opts)
-vim.keymap.set("n", "<leader>ch", "<Plug>(coc-codeaction)", Opts)
-
--- diagnostics
-vim.keymap.set("n", "<leader>cc", ":CocFzfList diagnostics<cr>", Opts)
-vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", Opts)
-vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", Opts)
-
--- actions
-vim.keymap.set("n", "<leader>cl",
-  ":<C-u>call CocActionAsync('codeLensAction')<CR>", Opts)
-
--- definitions
-vim.keymap.set("n", "gd", "<Plug>(coc-definition)zz", Opts)
-vim.keymap.set("n", "gy", "<Plug>(coc-type-definition)", Opts)
-vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", Opts)
-vim.keymap.set("n", "gr", "<Plug>(coc-references)", Opts)
 
 return {
   {
@@ -49,15 +26,15 @@ return {
     dependencies = {
       "tjdevries/coc-zsh",
       "antoinemadec/coc-fzf",
-    },
-    init = function ()
+     },
+    init = function()
       vim.g.coc_global_extensions = {
         "coc-tsserver",
         "coc-json",
         "coc-lua",
         "coc-yaml",
         "coc-pyright",
-      }
+       }
       vim.g.jsx_ext_required = 0 -- enable JSX syntax highlighing in javascript files
 
       vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
@@ -65,7 +42,33 @@ return {
 
       vim.cmd([[ command! -nargs=0 Prettier :CocCommand prettier.formatFile ]])
       vim.cmd([[ highlight link CocHighlightText Visual ]])
-      vim.cmd([[ inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>" ]])
+      vim.cmd(
+          [[ inoremap <expr> <cr> pumvisible() ? "\<C-y>" : "\<C-g>u\<CR>" ]])
+
+      -- definitions
+      vim.keymap.set("n", "gd", "<Plug>(coc-definition)zz", Opts)
+      vim.keymap.set("n", "gy", "<Plug>(coc-type-definition)", Opts)
+      vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", Opts)
+      vim.keymap.set("n", "gr", "<Plug>(coc-references)", Opts)
+
+      -- restart
+      vim.keymap.set("n", "<leader>cr", ":CocRestart<cr> ", Opts)
+      vim.keymap
+          .set("n", "<leader>tr", ":CocCommand tsserver.restart<cr>", Opts)
+
+      -- either doHover or show documentation in preview window
+      vim.keymap.set("n", "<leader>ck", function() show_documentation() end,
+                     Opts)
+      vim.keymap.set("n", "<leader>ch", "<Plug>(coc-codeaction)", Opts)
+
+      -- diagnostics
+      vim.keymap.set("n", "<leader>cc", ":CocFzfList diagnostics<cr>", Opts)
+      vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", Opts)
+      vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", Opts)
+
+      -- actions
+      vim.keymap.set("n", "<leader>cl",
+                     ":<C-u>call CocActionAsync('codeLensAction')<CR>", Opts)
 
       local au = vim.api.nvim_create_autocmd
       local aug = vim.api.nvim_create_augroup
@@ -73,53 +76,56 @@ return {
       -- coc
       aug("coc-custom", {
         clear = true,
-      })
+       })
       au("FileType", {
         pattern = "typescript,json",
         group = "coc-custom",
         command = "setl formatexpr=CocAction('formatSelected')",
         desc = "setup formatexpr specified filetype(s)",
-      })
+       })
       au("User", {
         pattern = "CocJumpPlaceholder",
         group = "coc-custom",
         command = "CocActionAsync('showSignatureHelp')",
         desc = "update signature help on jump placeholder",
-      })
+       })
       au("BufEnter", {
         desc = "disables autocommenting",
         command = "set fp-=c fo-=r fo-=o",
-      })
+       })
     end,
-  },
+   },
 
   -- lspconfig
   {
     "neovim/nvim-lspconfig",
     enabled = false,
-    opts = function ()
+    opts = function()
       local out = {
         servers = {
           lua_ls = {
             settings = {
               Lua = {
                 diagnostics = {
-                  globals = { "vim", 'Snacks' }, -- Make 'vim' a known global
-                },
+                  globals = {
+                    "vim",
+                    "Snacks",
+                   }, -- Make 'vim' a known global
+                 },
                 workspace = {
                   library = vim.api.nvim_get_runtime_file("", true),
                   checkThirdParty = false, -- (Optional) Disable third-party checks
-                },
+                 },
                 telemetry = {
                   enable = false, -- (Optional) Disable telemetry
-                },
-              },
-            },
-          },
-        },
-        setup = {}
-      }
+                 },
+               },
+             },
+           },
+         },
+        setup = {},
+       }
       return out
     end,
-  }
-}
+   },
+ }
