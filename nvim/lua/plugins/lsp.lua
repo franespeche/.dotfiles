98cfa8400aee104a654aa99aedd874611e8c7fea
frean <fespeche@gmail.com>
2025-08-24 17:57:51 -0300

    add lsp-config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,33 +1,33 @@
--- helpers
-local function show_documentation()
-  local filetype = vim.bo.filetype
-  local cword = vim.fn.expand("<cword>")
-
-  if vim.fn.index({
-    "help",
-   }, filetype) >= 0 then
-    vim.cmd("h " .. cword)
-  else
-    if vim.fn.index({
-      "lua",
-     }, filetype) >= 0 then
-      vim.lsp.buf.hover()
-    else
-      vim.fn.CocAction("doHover")
-    end
-  end
-end
-
 return {
   {
     "neoclide/coc.nvim",
     branch = "release",
-    enabled = true,
+    enabled = false,
     dependencies = {
       "tjdevries/coc-zsh",
       "antoinemadec/coc-fzf",
      },
     init = function()
+      -- helpers
+      local function show_documentation()
+        local filetype = vim.bo.filetype
+        local cword = vim.fn.expand("<cword>")
+
+        if vim.fn.index({
+          "help",
+         }, filetype) >= 0 then
+          vim.cmd("h " .. cword)
+        else
+          if vim.fn.index({
+            "lua",
+           }, filetype) >= 0 then
+            vim.lsp.buf.hover()
+          else
+            vim.fn.CocAction("doHover")
+          end
+        end
+      end
+
       vim.g.coc_global_extensions = {
         "coc-tsserver",
         "coc-json",
@@ -96,10 +96,39 @@ return {
     end,
    },
 
-  -- lspconfig
+  -- lspconfig: NOT NEEDED SINCE USING MASON-LSPCONFIG
   {
     "neovim/nvim-lspconfig",
-    enabled = false,
+    enabled = true,
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+     },
+    --[[
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- example: Lua (Neovim config)
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = {
+                "vim",
+               },
+             },
+            workspace = {
+              checkThirdParty = false,
+             },
+           },
+         },
+       })
+
+      -- add more servers as you install them
+      -- lspconfig.pyright.setup({})
+      -- lspconfig.tsserver.setup({})
+    end,
+    --[[
     opts = function()
       local out = {
         servers = {
@@ -127,5 +156,129 @@ return {
        }
       return out
     end,
+        ]] --
    },
+
+  -- mason
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+   },
+
+  -- Mason <-> LSPConfig bridge
+  {
+    "williamboman/mason-lspconfig.nvim",
+    enabled = true,
+    dependencies = {
+      "williamboman/mason.nvim",
+      "neovim/nvim-lspconfig",
+     },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      -- shared setup
+      local on_attach = function(client, bufnr)
+        local map = function(mode, lhs, rhs)
+          vim.keymap.set(mode, lhs, rhs, {
+            buffer = bufnr,
+           })
+        end
+        map("n", "gd", vim.lsp.buf.definition)
+        map("n", "K", vim.lsp.buf.hover)
+        map("n", "<leader>rn", vim.lsp.buf.rename)
+        map("n", "<leader>ca", vim.lsp.buf.code_action)
+      end
+
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- tell Mason which servers to auto-install and set up handlers
+      mason_lspconfig.setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "pyright",
+         },
+        handlers = {
+          -- default handler for all servers
+          function(server)
+            lspconfig[server].setup({
+              on_attach = on_attach,
+              capabilities = capabilities,
+             })
+          end,
+
+         },
+       })
+    end,
+   },
+
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip", -- for LuaSnip
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+     },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args) luasnip.lsp_expand(args.body) end,
+         },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({
+            select = true,
+           }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+           }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+           }),
+         }),
+        sources = {
+          {
+            name = "nvim_lsp",
+           },
+          {
+            name = "luasnip",
+           },
+          {
+            name = "buffer",
+           },
+          {
+            name = "path",
+           },
+         },
+       })
+    end,
+   },
+
  }
