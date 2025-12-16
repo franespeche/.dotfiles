52d9a04c19881ebac5c12d06d0bdda3c407aa797
frean <fespeche@gmail.com>
2025-12-16 09:27:19 -0300

    refactor telescope

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -1,45 +1,93 @@
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", Opts)
-vim.keymap.set("n", "<leader>fQ",
-               "<cmd>lua require('telescope.builtin').quickfixhistory{}<cr>",
-               Opts)
-vim.keymap.set("n", "<leader>fl",
-               "<cmd>lua require('telescope.builtin').loclist{}<cr>", Opts)
-vim.keymap.set("n", "<leader>fn",
-               "<cmd>lua require('telescope.builtin').find_files{ cwd = '~/.dotfiles/nvim/' }<cr>",
-               Opts)
-vim.keymap.set("n", "<leader>fng",
-               "<cmd>lua require('telescope.builtin').live_grep{ cwd = '~/.dotfiles/nvim/' }<cr>",
-               Opts)
--- vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", Opts) -- this is set in ./multigrep.lua
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", Opts)
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", Opts)
-vim.keymap.set("n", "<leader>fq", "<cmd>Telescope quickfix<cr>", Opts)
-vim.keymap.set("n", "<leader>fp",
-               ":lua require 'telescope.builtin'.current_buffer_fuzzy_find {}<CR>",
-               Opts)
-vim.keymap.set("n", "<leader>fz",
-               ":lua require 'telescope.builtin'.live_grep({ grep_open_fies=true })<CR>",
-               Opts)
-vim.keymap.set("n", "<leader>fc",
-               ":lua require 'telescope.builtin'.colorscheme {}<CR>", Opts)
-vim.keymap.set("n", "<Leader>fa",
-               ":lua require 'telescope.builtin'.autocommands{}<cr>", Opts)
-vim.keymap.set("n", "<Leader>fi",
-               ":lua require 'telescope.builtin'.highlights{}<cr>", Opts)
-
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "sharkdp/fd",
     "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope-ui-select.nvim",
    },
-  opts = function(_, opts)
+
+  keys = function()
+    local builtin = require("telescope.builtin")
+    local dotfiles = vim.fn.expand("~/.dotfiles/nvim/")
+
+    return {
+      {
+        "<leader>ff",
+        builtin.find_files,
+       },
+      {
+        "<leader>fQ",
+        builtin.quickfixhistory,
+       },
+      {
+        "<leader>fl",
+        builtin.loclist,
+       },
+      {
+        "<leader>fb",
+        builtin.buffers,
+       },
+      {
+        "<leader>fh",
+        builtin.help_tags,
+       },
+      {
+        "<leader>fq",
+        builtin.quickfix,
+       },
+      {
+        "<leader>fp",
+        builtin.current_buffer_fuzzy_find,
+       },
+      {
+        "<leader>fc",
+        builtin.colorscheme,
+       },
+      {
+        "<Leader>fa",
+        builtin.autocommands,
+       },
+      {
+        "<Leader>fi",
+        builtin.highlights,
+       },
+
+      {
+        "<leader>fn",
+        function()
+          builtin.find_files({
+            cwd = dotfiles,
+           })
+        end,
+       },
+      {
+        "<leader>fng",
+        function()
+          builtin.live_grep({
+            cwd = dotfiles,
+           })
+        end,
+       },
+
+      -- grep only open files
+      {
+        "<leader>fz",
+        function()
+          builtin.live_grep({
+            grep_open_files = true,
+           })
+        end,
+       },
+     }
+  end,
+
+  opts = function()
     local utils = require("telescope.utils")
     local actions = require("telescope.actions")
+    local themes = require("telescope.themes")
 
-    -- TODO: find a better place to call this
-    require"plugins.telescope.multigrep".setup()
+    require("plugins.telescope.multigrep").setup()
 
     return {
       defaults = {
@@ -64,82 +112,73 @@ return {
             ["<S-h>"] = "preview_scrolling_left",
             ["<C-c>"] = actions.close,
             ["<C-q>"] = "send_selected_to_qflist",
-            ["<Cr>"] = "select_default",
+            ["<CR>"] = "select_default",
            },
           i = {
             ["<C-j>"] = "move_selection_next",
             ["<C-k>"] = "move_selection_previous",
             ["<C-q>"] = "send_selected_to_qflist",
-            ["<Cr>"] = "select_default",
+            ["<CR>"] = "select_default",
            },
          },
+       },
+      extensions = {
+        ["ui-select"] = themes.get_dropdown({}),
        },
       pickers = {
         find_files = {
           path_display = {
             "truncate",
            },
-          -- wrap_results = true
          },
         live_grep = {
           results_title = false,
           path_display = {
             "truncate",
            },
-          -- theme = "dropdown",
-          -- wrap_results = true
          },
         buffers = {
           theme = "dropdown",
-          path_display = function(opts, path)
+          results_title = false,
+          path_display = function(_, path)
             local filename = utils.path_tail(path)
             local filepath = string.gsub(path, "/" .. filename, "")
             return string.format("%s (%s)", filename, filepath)
           end,
-          results_title = false,
-          -- theme = "dropdown",
-          -- wrap_results = true
          },
         location_list = {
-          path_display = function(opts, path)
+          results_title = false,
+          path_display = function(_, path)
             local tail = utils.path_tail(path)
             return string.format("%s (%s)", tail, path)
           end,
-          results_title = false,
-          -- theme = "dropdown",
-          -- wrap_results = true
          },
         quickfix = {
-          path_display = function(opts, path)
+          results_title = false,
+          path_display = function(_, path)
             local tail = utils.path_tail(path)
             return string.format("%s (%s)", tail, path)
           end,
-          results_title = false,
-          -- theme = "dropdown",
-          -- wrap_results = true
          },
         help_tags = {
+          results_title = false,
           path_display = {
             "truncate",
            },
-          results_title = false,
-          -- theme = "dropdown",
           layout_strategy = "vertical",
           layout_config = {
             height = 0.99,
             width = 0.99,
             preview_height = 0.68,
            },
-          -- theme = "dropdown",
-          -- wrap_results = true
          },
-        -- highlights = {
-        --   path_display = { "truncate" },
-        --   results_title = false,
-        --   -- theme = "dropdown",
-        --   -- wrap_results = true
-        --   },
        },
      }
+  end,
+
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("ui-select")
   end,
  }
