1c04087743a3fb9d0b631b1d521ccf397ce06869
frean <fespeche@gmail.com>
2025-12-16 09:27:15 -0300

    updates

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,14 +1,15 @@
 return {
   "github/copilot.vim",
-  init = function ()
+  enabled = true,
+  init = function()
     vim.g.copilot_filetypes = {
       vimwiki = false,
       json = false,
-    }
-    vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
+     }
+    vim.keymap.set("i", "<C-j>", "copilot#Accept(\"\\<CR>\")", {
       expr = true,
-      replace_keycodes = false
-    })
+      replace_keycodes = false,
+     })
     vim.g.copilot_no_tab_map = true
-  end
-}
+  end,
+ }
